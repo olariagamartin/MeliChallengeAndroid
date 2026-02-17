71a3b6f362949a576df6b365a83328e580f28793
martin.olariaga <martin.olariaga@geopagos.com>
2026-02-17 11:26:14 -0300

    chore: tests added to ProductDetailsVMTest

--- features/product-details/src/test/java/com/themarto/features/productDetails/ProductDetailsVMTest.kt
+++ features/product-details/src/test/java/com/themarto/features/productDetails/ProductDetailsVMTest.kt
@@ -3,12 +3,15 @@ package com.themarto.features.productDetails
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.mockito.kotlin.spy
+import org.mockito.kotlin.verify
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class ProductDetailsVMTest {
@@ -26,8 +29,30 @@ class ProductDetailsVMTest {
     }
 
     @Test
-    fun `0-WHEN ViewModel is created THEN loading is true`() = runTest {
-        val viewModel = ProductDetailsVM(repository = provideProductsRepository(), productId = "123")
+    fun `0-WHEN ViewModel is created THEN loading is true and product is null`() = runTest {
+        val viewModel = ProductDetailsVM(
+            repository = provideProductsRepository(), productId = "123"
+        )
         assert(viewModel.uiState.value.loading)
+        assert(viewModel.uiState.value.product == null)
+    }
+
+    @Test
+    fun `1-WHEN ViewModel is initialized THEN repository getProduct is called`() = runTest {
+        val repository = spy(provideProductsRepository())
+        val viewModel = ProductDetailsVM(
+            repository = repository, productId = "123"
+        )
+        advanceUntilIdle()
+        verify(repository).getProduct("123")
+    }
+
+    @Test
+    fun `2-WHEN repository return success THEN product is not null`() = runTest {
+        val viewModel = ProductDetailsVM(
+            repository = provideProductsRepository(), productId = "123"
+        )
+        advanceUntilIdle()
+        assert(viewModel.uiState.value.product != null)
     }
 }
