1699becbdad34aded75013298afdb0d9b9efe4fd
martin.olariaga <martin.olariaga@geopagos.com>
2026-03-31 00:06:44 -0300

    feature: add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,14 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.detekt)
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
+        buildUponDefaultConfig = true
+        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+    }
 }
